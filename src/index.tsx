ec0f74d5bd5f4f48472ab6a3797f25555285a873
Vlas <vlas.zub@gmail.com>
2024-04-19 20:45:19 +0300

    Some changes

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <BrowserRouter basename={"/"}>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
